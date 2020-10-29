33c7acbfe29db4fb30b5c9980908e37887a5a26b
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2020-10-29 22:02:01 +0200

    Project Created

---
+++ Entmaa Community Service/Entmaa Web Services/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Entmaa_Web_Services.MvcApplication" Language="C#" %>
---
+++ Entmaa Community Service/Entmaa Web Services/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Entmaa_Web_Services.Startup))]
+namespace Entmaa_Web_Services
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
