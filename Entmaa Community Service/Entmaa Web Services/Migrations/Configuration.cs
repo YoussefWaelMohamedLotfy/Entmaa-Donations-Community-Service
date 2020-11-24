7c0305131e7fe28d3d4f82bb19d9b017ba167295
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2020-11-24 13:11:59 +0200

    Database Setup Complete

---
+++ Entmaa Community Service/Entmaa Web Services/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Entmaa_Web_Services.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Entmaa_Web_Services.Persistence.MainContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Persistence.MainContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Entmaa Community Service/Entmaa Web Services/Persistence/MainContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Entmaa_Web_Services.Persistence
+{
+    public class MainContext : DbContext
+    {
+        
+
+        public MainContext() : base("name=EntmaaConnection")
+        {
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // To be removed
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
