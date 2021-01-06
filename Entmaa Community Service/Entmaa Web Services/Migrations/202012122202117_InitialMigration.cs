58f06c23541bf04e251ac48976f4be5b738a0bd6
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2021-01-06 20:19:30 +0200

    Added 2 packages from NuGet

---
+++ Entmaa Community Service/Entmaa Web Services/Migrations/202012122202117_InitialMigration.cs
@@ -0,0 +1,16 @@
+﻿namespace Entmaa_Web_Services.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
