575eff3c50b2b60daeadab15f1a37a6c9c17c818
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2021-01-10 21:25:29 +0200

    Modified bundles

--- Entmaa Community Service/Entmaa Web Services/App_Start/BundleConfig.cs
+++ Entmaa Community Service/Entmaa Web Services/App_Start/BundleConfig.cs
@@ -8,8 +8,13 @@ namespace Entmaa_Web_Services
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootbox.js",
+                        "~/Scripts/toastr.js",
+                        "~/Scripts/DataTables/jquery.datatables.js",
+                        "~/Scripts/DataTables/datatables.bootstrap4.js",
+                        "~/Scripts/bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -19,11 +24,11 @@ namespace Entmaa_Web_Services
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-flatly.css",
+                      "~/Content/bootstrap-darkly.css",
+                      "~/Content/DataTables/css/datatables.bootstrap4.css",
+                      "~/Content/toastr.css",
                       "~/Content/site.css"));
         }
     }
--- Entmaa Community Service/Entmaa Web Services/App_Start/FilterConfig.cs
+++ Entmaa Community Service/Entmaa Web Services/App_Start/FilterConfig.cs
@@ -8,6 +8,8 @@ namespace Entmaa_Web_Services
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
