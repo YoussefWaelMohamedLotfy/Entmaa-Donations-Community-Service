605570d033515485dfd1f5dfe58aaa59e9f5664a
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2021-01-17 22:55:40 +0200

    Separated City_CountryConfiguration

--- Entmaa Community Service/Entmaa Web Services/Persistence/EntityConfigurations/CityConfiguration.cs
+++ Entmaa Community Service/Entmaa Web Services/Persistence/EntityConfigurations/CityConfiguration.cs
@@ -7,16 +7,6 @@ using Entmaa_Web_Services.Models.Entmaa;
 
 namespace Entmaa_Web_Services.Persistence.EntityConfigurations
 {
-    public class CountryConfiguration : EntityTypeConfiguration<Country>
-    {
-        public CountryConfiguration()
-        {
-            Property(c => c.Name).IsRequired();
-
-            HasMany(c => c.Cities).WithRequired(c => c.Country).HasForeignKey(c => c.CountryID);
-        }
-    }
-
     public class CityConfiguration : EntityTypeConfiguration<City>
     {
         public CityConfiguration()
---
+++ Entmaa Community Service/Entmaa Web Services/Persistence/EntityConfigurations/CountryConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+using Entmaa_Web_Services.Models.Entmaa;
+
+namespace Entmaa_Web_Services.Persistence.EntityConfigurations
+{
+    public class CountryConfiguration : EntityTypeConfiguration<Country>
+    {
+        public CountryConfiguration()
+        {
+            Property(c => c.Name).IsRequired();
+
+            HasMany(c => c.Cities).WithRequired(c => c.Country).HasForeignKey(c => c.CountryID);
+        }
+    }
+}
