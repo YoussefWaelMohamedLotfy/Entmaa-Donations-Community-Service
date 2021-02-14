6166d7dbacee228c0fc883ef5e9e72a6d602a23c
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2021-02-15 00:19:36 +0200

    Fixed Package Exception at Startup

---
+++ Entmaa Community Service/Entmaa Web Services/ViewModels/FirebaseNotification.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
+
+namespace Entmaa_Web_Services.ViewModels
+{
+    public class MobileMessagingClient
+    {
+        private readonly FirebaseMessaging messaging;
+
+        public MobileMessagingClient()
+        {
+            var app = FirebaseApp.Create(new AppOptions() { Credential = GoogleCredential.FromFile("serviceAccountKey.json").CreateScoped("https://www.googleapis.com/auth/firebase.messaging") });
+            messaging = FirebaseMessaging.GetMessaging(app);
+        }
+        //...     
+
+        private Message CreateNotification(string title, string notificationBody, string token)
+        {
+            return new Message()
+            {
+                Token = token,
+                Notification = new Notification()
+                {
+                    Body = notificationBody,
+                    Title = title,
+                }
+            };
+        }
+
+        public async Task<string> SendNotification(string token, string title, string body)
+        {
+            var result = await messaging.SendAsync(CreateNotification(title, body, token));
+            //do something with result
+            return result;
+        }
+    }
+
+    public class FirebaseNotification
+    {
+
+    }
+}
