a4524a783adae37218ea4b1c087c61741ab190b9
ahmed_safwat <42112466+Ahmedsafwat101@users.noreply.github.com>
2021-04-09 20:52:11 +0200

    wizardactivity

--- Entmaa/app/src/main/java/com/team/entmaa/ui/authactivity/authfragment/SignupFragment.kt
+++ Entmaa/app/src/main/java/com/team/entmaa/ui/authactivity/authfragment/SignupFragment.kt
@@ -1,11 +1,14 @@
 package com.team.entmaa.ui.authactivity.authfragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.team.entmaa.R
+import com.team.entmaa.ui.DataActivity
 
 class SignupFragment:Fragment() {
     override fun onCreateView(
@@ -13,7 +16,15 @@ class SignupFragment:Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.signup_fragment, container, false)
+        val rootView = inflater.inflate(R.layout.signup_fragment, container, false)
+
+        //Currently will redirect to the mainPage
+        val loginBut: Button = rootView.findViewById(R.id.signup_button)
+        loginBut.setOnClickListener {
+            val intent: Intent = Intent(activity, DataActivity::class.java)
+            startActivity(intent)
+        }
+        return rootView
     }
 
 
