229bb92f4f7ef52eb1821c181f7c0542263bc5c1
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2021-04-16 10:20:07 +0200

    Added Contributor Repository

---
+++ Entmaa Community Service/Entmaa Web Services/Persistence/Repositories/ContributorRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Entmaa_Web_Services.Core.Repositories;
+using Entmaa_Web_Services.Models.Entmaa;
+
+namespace Entmaa_Web_Services.Persistence.Repositories
+{
+    public class ContributorRepository : Repository<Contributor>, IContributorRepository
+    {
+        public MainContext MainContext { get { return _context as MainContext; } }
+
+        public ContributorRepository(MainContext context) : base(context)
+        {
+                
+        }
+
+        public Contributor GetContributorProfile(int id)
+        {
+            return MainContext.Contributors
+                .Include(c => new { c.Tags, c.Locations })
+                .SingleOrDefault(c => c.ID == id);
+        }
+    }
+}
