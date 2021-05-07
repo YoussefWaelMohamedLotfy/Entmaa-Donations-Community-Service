f66c67bf736ebc50eaae35ced192a122ca7c6cec
ahmed_safwat <42112466+Ahmedsafwat101@users.noreply.github.com>
2021-05-08 00:23:22 +0200

    map

--- Entmaa/app/src/main/java/com/team/entmaa/ui/MapsFragment.kt
+++ Entmaa/app/src/main/java/com/team/entmaa/ui/MapsFragment.kt
@@ -1,35 +1,54 @@
 package com.team.entmaa.ui
 
-import androidx.fragment.app.Fragment
-
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.SearchView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.team.entmaa.R
+import java.io.IOException
+
 
 class MapsFragment : Fragment() {
 
+    private var mMap: GoogleMap? = null
+    internal var mCurrLocationMarker: Marker? = null
+    internal lateinit var mLocationRequest: LocationRequest
+    internal var mGoogleApiClient: GoogleApiClient? = null
+    var addressList: List<Address>? = null
+
+
+    lateinit var searchView:SearchView
+
+
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val Egypt = LatLng(30.0444196,31.2357116)
+        googleMap.addMarker(MarkerOptions().position(Egypt).title("Egypt"))
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(Egypt, 15f))
+
     }
 
     override fun onCreateView(
@@ -37,12 +56,65 @@ class MapsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+        val root= inflater.inflate(R.layout.fragment_maps, container, false)
+        Log.i("here","onCreateView")
+
+        searchView =  root.findViewById(R.id.searchBar)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(location: String?): Boolean {
+                if (location == null || location == "") {
+                    Toast.makeText(activity,"provide location",Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    val geoCoder = Geocoder(activity)
+                    try {
+                        addressList = geoCoder.getFromLocationName(location, 1)
+
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+
+                    //Log.i("map",location)
+
+                    if(addressList==null || addressList!!.isEmpty()) {
+                        Toast.makeText(activity,"Please rewrite the location correctly ;)",Toast.LENGTH_SHORT).show()
+                        return false
+                    }
+                    val address = addressList!![0]
+
+                    Log.i("map",address!!.latitude.toString())
+                    Log.i("map",address!!.longitude.toString())
+
+                    val latLng = LatLng(address.latitude, address.longitude)
+
+                    Log.i("map","here")
+                    val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+                    mapFragment?.getMapAsync { googleMap ->
+                        googleMap.clear()
+                        googleMap.addMarker(MarkerOptions().position(latLng).title(location))
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+                    }
+                }
+                return false
+            }
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false;
+
+            }
+        })
+
+        return  root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.i("here","onViewCreated")
+
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
     }
+
+
+
+
 }
