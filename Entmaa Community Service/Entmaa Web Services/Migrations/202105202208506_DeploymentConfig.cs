dd5a221ca763c20f6abe8c210853a796e90a1712
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2021-05-21 00:45:37 +0200

    Fixed Deployment Error

---
+++ Entmaa Community Service/Entmaa Web Services/Migrations/202105202208506_DeploymentConfig.cs
@@ -0,0 +1,20 @@
+﻿namespace Entmaa_Web_Services.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DeploymentConfig : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Organizations", "Rating", c => c.Single(nullable: false));
+            AlterColumn("dbo.Reviews", "Rating", c => c.Single(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Reviews", "Rating", c => c.Double(nullable: false, storeType: "real"));
+            AlterColumn("dbo.Organizations", "Rating", c => c.Double(nullable: false, storeType: "real"));
+        }
+    }
+}
